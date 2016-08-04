2b4f25637ecfa17d51a174841b23e39299477af2
unknown <bubu19990316@gmail.com>
2016-08-04 05:57:19 +0200

    Retry on network error, and return with exit code 1 on error

--- rechat-dl.py
+++ rechat-dl.py
@@ -7,6 +7,9 @@ import time
 import math
 import json
 
+CHUNK_ATTEMPTS = 6
+CHUNK_ATTEMPT_SLEEP = 10
+
 if len(sys.argv) < 2 or len(sys.argv) > 3:
     print("usage:")
     print("  rechat-dl.py VOD-ID [FILE]")
@@ -26,8 +29,7 @@ if len(sys.argv) == 3:
    file_name = sys.argv[2] 
 
 if "error" in vod_info:
-    print("got an error when getting vod info: " + str(vod_info))
-    exit(0)
+    sys.exit("got an error in vod info response: " + str(vod_info))
 
 start_timestamp = calendar.timegm(time.strptime(vod_info["recorded_at"], "%Y-%m-%dT%H:%M:%SZ"))
 video_len = int(vod_info["length"])
@@ -37,12 +39,33 @@ vod_info['start_timestamp'] = start_timestamp
 messages.append(vod_info)   # we store the vod metadata in the first element of the message array
 
 for chat_timestamp in range(start_timestamp, last_timestamp + 1, 30):
-    print("\rdownloading chunk " + str(int((chat_timestamp - start_timestamp) / 30)) + " / " + str(int((last_timestamp - start_timestamp) / 30)), end="")
+    chunk_number = int((chat_timestamp - start_timestamp) / 30) + 1
+    chunks = int((last_timestamp - start_timestamp) / 30) + 1
     
-    chat_json = requests.get("http://rechat.twitch.tv/rechat-messages?start=" + str(chat_timestamp) + "&video_id=v" + sys.argv[1]).json()
+    print("\rdownloading chunk " + str(chunk_number) + " / " + str(chunks), end="")
+    
+    chat_json = None
+    
+    for i in range(0, CHUNK_ATTEMPTS):
+        try:
+            chat_json = requests.get("http://rechat.twitch.tv/rechat-messages?start=" + str(chat_timestamp) + "&video_id=v" + sys.argv[1]).json()
+        except requests.exceptions.ConnectionError as e:
+            print("\nerror while downloading chunk #" + str(chunk_number) + ": " + str(e))
+            
+            if i < CHUNK_ATTEMPTS - 1:
+                    print("retrying in " + str(CHUNK_ATTEMPT_SLEEP) + " seconds", end="")
+            print(" (attempt " + str(i + 1) + "/" + str(CHUNK_ATTEMPTS) + ")")
+            
+            if i < CHUNK_ATTEMPTS - 1:
+                time.sleep(CHUNK_ATTEMPT_SLEEP)
+        else:
+            break
+    
+    if chat_json == None:
+        sys.exit("max retries exceeded.")
+    
     if "errors" in chat_json or not "data" in chat_json:
-        print("got an error when getting chat messages: " + str(chat_json))
-        exit(0)
+        sys.exit("got an error in chat message response: " + str(chat_json))
     
     messages += chat_json["data"]
 
